b22bd7ff053456f0994b2606a35a67e681417d36
EC2 Default User <ec2-user@ip-172-31-37-161.us-east-2.compute.internal>
2018-04-28 19:00:46 +0000

    finished project

---
+++ src/components/Secret.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="secret">
+    <h1>{{ message }}</h1>
+    <ul>
+       <li><router-link v-bind:to="{ name: 'Home' }">Home</router-link></li>
+      <li><router-link v-bind:to="{ name: 'Survey' }">Survey</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Secret',
+  data () {
+    return {
+      message: "Always look in the devtools"
+    }
+  }
+ }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error {
+  border: 1px solid #aa0000;
+  padding: 1rem;
+  color: #aa0000;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
